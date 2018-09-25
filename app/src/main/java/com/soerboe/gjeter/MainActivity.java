5dee73df55e2049aeb991d73bae7f047afaa36d4
Ivar Sorbo <ivar-so@hotmail.com>
2018-09-25 11:39:38 +0200

    simple MapView with a OSM map

--- app/src/main/java/com/soerboe/gjeter/MainActivity.java
+++ app/src/main/java/com/soerboe/gjeter/MainActivity.java
@@ -1,13 +1,68 @@
 package com.soerboe.gjeter;
 
+import android.Manifest;
+import android.content.Context;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //TODO Handle permissions (location access is only needed if GPS is used. Write access is needed for all use of the app)
+        /*
+        String[] reqPermission = new String[] {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+                */
+
+
+        // Load/initialize the osmdroid configuration
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        // Create the map
         setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.map);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        // Add default zoom buttons and ability to zoom with 2 fingers.
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+
+        // Move the map to the starting position.
+        GeoPoint startPoint = new GeoPoint(63.419780, 10.401765);
+        moveMapTo(startPoint);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+
+    private void moveMapTo(GeoPoint point){
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(point);
     }
 }
