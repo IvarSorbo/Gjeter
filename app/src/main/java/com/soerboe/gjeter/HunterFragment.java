c8d6327600e5ffc7191f8def7c2f12a445ce61ca
Ivar Sorbo <ivar-so@hotmail.com>
2018-10-28 10:59:39 +0100

    hunter observation

--- app/src/main/java/com/soerboe/gjeter/HunterFragment.java
+++ app/src/main/java/com/soerboe/gjeter/HunterFragment.java
@@ -1,22 +1,52 @@
 package com.soerboe.gjeter;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+
 public class HunterFragment extends MyFragment {
+
+    private TextInputEditText hunterCount, dogCount;
+    private Activity activity;
+    private HunterObservation hunterObservation;
+    private Gson gson = new Gson();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_hunter, container, false);
+        hunterCount = view.findViewById(R.id.hunters_hunterFragment);
+        dogCount = view.findViewById(R.id.dogs_hunterFragment);
+
+        // Assumes that the parent activity is an ObservationActivity
+        Observation observation = ((ObservationActivity) activity).getObservation();
+        hunterObservation = new HunterObservation(observation);
+
         return view;
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.activity = getActivity();
+    }
+
+    private void update(){
+        hunterObservation.setHunterCount(InputChecker.getInt(hunterCount,0 ));
+        hunterObservation.setDogCount(InputChecker.getInt(dogCount, 0));
+    }
+
+    @Override
     public String toJSON() {
-        return "Hello from HunterFragment";
+        update();
+        return gson.toJson(hunterObservation);
     }
 }
---
+++ app/src/main/java/com/soerboe/gjeter/HunterObservation.java
@@ -0,0 +1,26 @@
+package com.soerboe.gjeter;
+
+public class HunterObservation extends Observation {
+    private int hunterCount;
+    private int dogCount;
+
+    public HunterObservation(Observation o) {
+        super(o);
+    }
+
+    public int getHunterCount() {
+        return hunterCount;
+    }
+
+    public int getDogCount() {
+        return dogCount;
+    }
+
+    public void setHunterCount(int hunterCount) {
+        this.hunterCount = hunterCount;
+    }
+
+    public void setDogCount(int dogCount) {
+        this.dogCount = dogCount;
+    }
+}
